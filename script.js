beaf67f71a90d73014dba9afb544d87a6a49a3e2
Kike Algarra <kike@areavisual.net>
2019-12-02 09:46:06 +0100

    Hacia arriba
    Hacia arriba.

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){ //Hacia arriba
+  irArriba();
+});
+
+function irArriba(){
+  $('.ir-arriba').click(function(){ $('body,html').animate({ scrollTop:'0px' },1000); });
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 0){ $('.ir-arriba').slideDown(600); }else{ $('.ir-arriba').slideUp(600); }
+  });
+  $('.ir-abajo').click(function(){ $('body,html').animate({ scrollTop:'1000px' },1000); });
+}
